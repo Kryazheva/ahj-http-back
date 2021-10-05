748c5ad74c4924f02612cdc392c9f9f1286ade93
kristina <kistina@localhost>
2021-10-05 13:47:09 +0300

    start server

--- server.js
+++ server.js
@@ -67,4 +67,4 @@ app.use(router.routes()).use(router.allowedMethods());
 
 const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback());
-server.listen(port, () => console.log('Server started'));
+server.listen(port, () => console.log('Server OK'));
